d0e57a76593ebd88be7f2433d3da19cf4d3d8c6d
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-15 10:22:54 +0300

    Add project files.

---
+++ Encapsulation/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Point
+    {
+        double x;
+        double y;       
+        public double GetX()
+        { return x; }
+        public double GetY() { return y; }
+        public void SetX(double x) { this.x = x; }
+        public void SetY(double y) {  this.y = y; }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack - статическая память;
+            //Heap - динамическая память;
+
+            Point A = new Point(); // Оператор new вызывает конструктор класса
+            A.
+        }
+    }
+}
