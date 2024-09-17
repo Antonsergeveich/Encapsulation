8fcf86a0138dab914736dd2a839b81ed46a589ef
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-17 20:44:28 +0300

    +

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    internal class Fraction
+    {
+        public int Integer { get; set; }
+        //Автосвойства, не требуют объявления переменной, поскольку сами неявно её объявляют.
+        public int Numerator { get; set; }
+        int denominator;
+        // Обычные свойства, которые применяются к переменной 'denominator':
+        public int Denominator
+        {
+            get => denominator;
+            set => denominator = value == 0 ? 1 : value;
+            // Если метод реализуется одним выражением его можно реализовать без фигурных скобок
+            // Реализация пишется после знака '=>'
+            // https://learn.microsoft.com/ru-ru/dotnet/csharp/language-reference/operators/lambda-operator
+            // '=>' подразумевает возврат указанного значения
+        }
+
+        //            Constructors:
+
+        public Fraction()
+        {
+            Console.WriteLine($"DefConstructor:\t{GetHashCode()}");
+        }
+        public Fraction(int integer)
+        {
+            this.Integer = integer;
+            Console.WriteLine($"1ArgConstructor:{GetHashCode()}");
+            //https://learn.microsoft.com/ru-ru/dotnet/api/system.object.gethashcode?view=net-8.0
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+            Console.WriteLine($"Constructor:\t{GetHashCode()}");
+        }
+        ~Fraction()
+        {
+            Console.WriteLine($"Destructor:\t{GetHashCode()}");
+        }
+
+        //              Methods:
+        public void Print()
+        {
+            if (Integer != 0) Console.WriteLine(Integer);
+            if (Numerator != 0)
+            {
+                if (Integer != 0) Console.Write("(");
+                Console.Write($"{Numerator} / {Denominator}");
+                if (Integer != 0) Console.Write(")");
+            }
+            else if (Integer == 0) Console.Write(0);
+            Console.WriteLine();
+        }
+    }
+}
